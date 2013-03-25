5397fa8023fa2fcabed3541c1bd3d3d60941cb10
aisk <aisk1988@gmail.com>
2013-03-25 12:05:14 +0800

    add echo server example

---
+++ example/echo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "../ae.c"
+#include "../anet.c"
+
+aeEventLoop *loop;
+
+void readFromClient(aeEventLoop *el, int fd, void *privdata, int mask) {
+    int buffer_size = 1024;
+    char buffer[1024];
+    int size;
+    size = read(fd, buffer, buffer_size);
+    write(fd, buffer, size);
+}
+
+void acceptTcpHandler(aeEventLoop *el, int fd, void *privdata, int mask) {
+    int client_port, client_fd;
+    char client_ip[128];
+    // create client socket
+    client_fd = anetTcpAccept(NULL, fd, client_ip, &client_port);
+    printf("Accepted %s:%d\n", client_ip, client_port);
+
+    // set client socket non-block
+    anetNonBlock(NULL, client_fd);
+    anetTcpNoDelay(NULL, client_fd);
+
+    // regist on message callback
+    int ret;
+    ret = aeCreateFileEvent(loop, client_fd, AE_READABLE, readFromClient, NULL);
+    assert(ret != AE_ERR);
+}
+
+int main()
+{
+    int ipfd;
+    // create server socket
+    ipfd = anetTcpServer(NULL, 8000, "0.0.0.0");
+    assert(ipfd != ANET_ERR);
+
+    // create main event loop
+    loop = aeCreateEventLoop();
+
+    // regist socket connect callback
+    int ret;
+    ret = aeCreateFileEvent(loop, ipfd, AE_READABLE, acceptTcpHandler, NULL);
+    assert(ret != AE_ERR);
+
+    // start main loop
+    aeMain(loop);
+
+    return 0;
+}
